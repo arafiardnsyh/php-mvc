6d315b30a63e5430ae8ddf71bb5ad4dac176fd38
arafiardnsyh <arafiardnsyh@gmail.com>
2022-06-04 20:19:33 +0700

    materi Database Wrapper

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -3,6 +3,16 @@
 	<div class="row">
 		<div class="col-6">
 			<h3>Daftar Mahasiswa</h3>
+			<ul class="list-group ">
+				<?php foreach ($data['mhs'] as $mhs) : ?>
+					<li class="list-group-item d-flex justify-content-between align-items-center">
+						<?= ucwords($mhs['nama']) ?>
+						<a href="<?= BASEURL ?>/mahasiswa/detail/<?= $mhs
+						['id']; ?>" class="badge badge-primary ">detail</a>	
+					</li>
+				<?php endforeach;?>
+			</ul>
+			<br><br>
 			<?php foreach ($data['mhs'] as $mhs) : ?>
 				<ul>
 					<li><?= $mhs['nama'] ?></li>
