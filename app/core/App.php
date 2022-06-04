ee0f10d64c65d3265ffd1da698c1c6290ee7e75e
arafiardnsyh <arafiardnsyh@gmail.com>
2022-06-04 08:47:57 +0700

    mengubah file Home.php

--- app/core/App.php
+++ app/core/App.php
@@ -30,13 +30,14 @@ class App {
 			}
 		}
 
-		//params
+		//params = parameter
 		if (!empty($url)) {
 			$this->params = array_values($url);
 		}
 
 		//jalankan controller & method, serta kirimkan params jika ada
 		call_user_func_array([$this->controller, $this->method], $this->params);
+		call_user_func_array(function, param_arr)
 	}
 
 	public function parseURL()
