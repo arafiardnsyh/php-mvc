469535f697a267da3f00ce47664b53fcfd622825
arafiardnsyh <arafiardnsyh@gmail.com>
2022-06-07 15:20:53 +0700

    materi Insert Data

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,7 +1,11 @@
-<div class="container">
+<div class="container mt-3">
 	
 	<div class="row">
 		<div class="col-6">
+		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModel">
+		  Tambah Data Mahasiswa
+		</button>
+		<br><br>
 			<h3>Daftar Mahasiswa</h3>
 			<ul class="list-group ">
 				<?php foreach ($data['mhs'] as $mhs) : ?>
@@ -12,16 +16,54 @@
 					</li>
 				<?php endforeach;?>
 			</ul>
-			<br><br>
-			<?php foreach ($data['mhs'] as $mhs) : ?>
-				<ul>
-					<li><?= $mhs['nama'] ?></li>
-					<li><?= $mhs['nrp'] ?></li>
-					<li><?= $mhs['email'] ?></li>
-					<li><?= $mhs['jurusan'] ?></li>
-				</ul>
-			<?php endforeach;?>
 		</div>
 	</div>
 
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="formModel" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Tambah Data Mahasiswa</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="<?= BASEURL ?>/mahasiswa/tambah" method="POST">
+        	<div class="form-group">
+			    <label for="nama">Nama</label>
+			    <input type="text" class="form-control" id="nama" name="nama">
+			</div>
+
+        	<div class="form-group">
+			    <label for="nrp">NRP</label>
+			    <input type="number" class="form-control" id="nrp" name="nrp">
+			</div>
+
+        	<div class="form-group">
+			    <label for="email">Email</label>
+			    <input type="email" class="form-control" id="email" name="email">
+			</div>
+
+			<div class="form-group">
+				<label for="jurusan">Jurusan</label>
+				<select class="form-control" id="jurusan" name="jurusan">
+				  <option value="Teknik Informatika">Teknik Informatika</option>
+				  <option value="Teknik Komputer">Teknik Komputer</option>
+				  <option value="Teknik Sipil">Teknik Sipil</option>
+				  <option value="Teknik Pangan">Teknik Pangan</option>
+				  <option value="Teknik Kehutanan">Teknik Kehutanan</option>
+				</select>
+			</div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
+	    </form>
+      </div>
+    </div>
+  </div>
 </div>
