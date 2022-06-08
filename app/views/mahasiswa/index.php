821f68d452388c9be2806b12a1a45e54c6b89e01
arafiardnsyh <arafiardnsyh@gmail.com>
2022-06-08 11:58:19 +0700

    materi Searching

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -5,13 +5,30 @@
 			<?php Flasher::flash(); ?> 
 		</div>
 	</div>
+
+	<div class="row mb-3">
+		<div class="col-6">
+			<button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModel">
+			  Tambah Data Mahasiswa
+			</button>
+		</div>
+	</div>
+
+	<div class="row mb-3">
+		<div class="col-6">
+			<form action="<?= BASEURL ?>/mahasiswa/cari" method="POST">
+				<div class="input-group ">
+					<input type="text" class="form-control" placeholder="cari mahasiswa..." name="keyword" id="keyword" autocomplete="off">
+					<div class="input-group-append">
+						<button class="btn btn-primary" type="submit" id="tombolCari">Cari</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
 	
 	<div class="row">
 		<div class="col-6">
-		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModel">
-		  Tambah Data Mahasiswa
-		</button>
-		<br><br>
 			<h3>Daftar Mahasiswa</h3>
 			<ul class="list-group ">
 				<?php foreach ($data['mhs'] as $mhs) : ?>
@@ -19,6 +36,9 @@
 						<?= ucwords($mhs['nama']) ?>
 						<a href="<?= BASEURL ?>/mahasiswa/hapus/<?= $mhs
 						['id']; ?>" class="badge badge-danger float-right ml-1" onclick="return confirm('yakin?')">hapus</a>	
+						<a href="<?= BASEURL ?>/mahasiswa/ubah/<?= $mhs
+						['id']; ?>" class="badge badge-success float-right ml-1 tampilModalUbah" data-toggle="modal" data-target="#formModel" data-id="<?= $mhs
+						['id']; ?>">ubah</a>		
 						<a href="<?= BASEURL ?>/mahasiswa/detail/<?= $mhs
 						['id']; ?>" class="badge badge-primary float-right ml-1 ">detail</a>	
 					</li>
@@ -30,17 +50,18 @@
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="formModel" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="formModel" tabindex="-1" aria-labelledby="formModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Tambah Data Mahasiswa</h5>
+        <h5 class="modal-title" id="formModalLabel">Tambah Data Mahasiswa</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
         <form action="<?= BASEURL ?>/mahasiswa/tambah" method="POST">
+        	<input type="hidden" name="id" id="id">
         	<div class="form-group">
 			    <label for="nama">Nama</label>
 			    <input type="text" class="form-control" id="nama" name="nama">
